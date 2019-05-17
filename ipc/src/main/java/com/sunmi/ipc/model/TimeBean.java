e7b0d126c56890495e14605973946e82d2f3768d
yangshijie <yangshijie@sunmi.com>
2019-05-17 10:20:26 +0800

    fix bug

---
+++ ipc/src/main/java/com/sunmi/ipc/model/TimeBean.java
@@ -0,0 +1,25 @@
+package com.sunmi.ipc.model;
+
+/**
+ * Created by YangShiJie on 2019/4/29.
+ */
+public class TimeBean {
+    private String time;
+    private long date;
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public long getDate() {
+        return date;
+    }
+
+    public void setDate(long date) {
+        this.date = date;
+    }
+}
