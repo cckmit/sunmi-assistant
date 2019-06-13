eff8bed084224b1fb2de25357f12d50c3a971d49
yinhui <yinhui@sunmi.com>
2019-06-13 16:55:24 +0800

    Add data fragment(empty) at home page.

---
+++ app/src/main/java/com/sunmi/assistant/contract/DataContract.java
@@ -0,0 +1,14 @@
+package com.sunmi.assistant.contract;
+
+import sunmi.common.base.BaseView;
+
+public interface DataContract {
+
+    interface View extends BaseView {
+
+    }
+
+    interface Presenter {
+
+    }
+}
---
+++ app/src/main/java/com/sunmi/assistant/presenter/DataPresenter.java
@@ -0,0 +1,8 @@
+package com.sunmi.assistant.presenter;
+
+import com.sunmi.assistant.contract.DataContract;
+
+import sunmi.common.base.BasePresenter;
+
+public class DataPresenter extends BasePresenter<DataContract.View> implements DataContract.Presenter {
+}
