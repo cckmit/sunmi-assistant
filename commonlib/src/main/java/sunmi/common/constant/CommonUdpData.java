4f7a13993f01e4b52c6ae2f1bd210f2c6e8fee96
yangshijie <yangshijie@sunmi.com>
2019-07-19 11:16:38 +0800

    ipc setting

--- commonlib/src/main/java/sunmi/common/constant/CommonUdpData.java
+++ commonlib/src/main/java/sunmi/common/constant/CommonUdpData.java
@@ -1,7 +1,8 @@
 package sunmi.common.constant;
 
-import java.util.ArrayList;
-import java.util.List;
+import android.util.ArrayMap;
+
+import java.util.Map;
 
 import sunmi.common.model.SunmiDevice;
 
@@ -9,64 +10,47 @@ import sunmi.common.model.SunmiDevice;
  * Created by YangShiJie on 2019/7/17.
  */
 public class CommonUdpData {
-    public static List<SunmiDevice> udpList = new ArrayList<>();
+    public static Map<String, SunmiDevice> sunmiDevMap = new ArrayMap<>();
 
     public static String currentRouterDeviceId(String deviceid) {
-        if (udpList == null || udpList.size() == 0) {
+        if (sunmiDevMap == null) {
             return "";
         }
-        for (SunmiDevice sd : udpList) {
-            if ("ROUTER".equalsIgnoreCase(sd.getType()) && sd.getDeviceid().equalsIgnoreCase(deviceid)) {
-                return sd.getDeviceid();
-            }
+        if (CommonUdpData.sunmiDevMap.containsKey(deviceid)) {
+            return CommonUdpData.sunmiDevMap.get(deviceid).getDeviceid();
         }
         return "";
     }
 
     public static String currentFactory(String deviceid) {
-        if (udpList == null || udpList.size() == 0) {
+        if (sunmiDevMap == null) {
             return "";
         }
-        for (SunmiDevice sd : udpList) {
-            if ("ROUTER".equalsIgnoreCase(sd.getType()) && sd.getDeviceid().equalsIgnoreCase(deviceid)) {
-                return sd.getFactory();
-            }
+        if (CommonUdpData.sunmiDevMap.containsKey(deviceid) &&
+                CommonUdpData.sunmiDevMap.get(deviceid).getType().equalsIgnoreCase("ROUTER")) {
+            return CommonUdpData.sunmiDevMap.get(deviceid).getFactory();
         }
         return "";
     }
 
     public static String currentIpcDeviceId(String deviceid) {
-        if (udpList == null || udpList.size() == 0) {
+        if (sunmiDevMap == null) {
             return "";
         }
-        for (SunmiDevice sd : udpList) {
-            if ("IPC".equalsIgnoreCase(sd.getType()) && sd.getDeviceid().equalsIgnoreCase(deviceid)) {
-                return sd.getDeviceid();
-            }
+        if (CommonUdpData.sunmiDevMap.containsKey(deviceid) &&
+                CommonUdpData.sunmiDevMap.get(deviceid).getType().equalsIgnoreCase("IPC")) {
+            return CommonUdpData.sunmiDevMap.get(deviceid).getDeviceid();
         }
         return "";
     }
 
     public static String currentIpcIp(String deviceid) {
-        if (udpList == null || udpList.size() == 0) {
+        if (sunmiDevMap == null) {
             return "";
         }
-        for (SunmiDevice sd : udpList) {
-            if ("IPC".equalsIgnoreCase(sd.getType()) && sd.getDeviceid().equalsIgnoreCase(deviceid)) {
-                return sd.getIp();
-            }
-        }
-        return "";
-    }
-
-    public static String currentIpcModel(String deviceid) {
-        if (udpList == null || udpList.size() == 0) {
-            return "";
-        }
-        for (SunmiDevice sd : udpList) {
-            if ("IPC".equalsIgnoreCase(sd.getType()) && sd.getDeviceid().equalsIgnoreCase(deviceid)) {
-                return sd.getModel();
-            }
+        if (CommonUdpData.sunmiDevMap.containsKey(deviceid) &&
+                CommonUdpData.sunmiDevMap.get(deviceid).getType().equalsIgnoreCase("IPC")) {
+            return CommonUdpData.sunmiDevMap.get(deviceid).getIp();
         }
         return "";
     }
