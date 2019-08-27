17d13e94f0871a5e22a5ccb71fae4a0368ffd9c8
林远鹏 <linyuanpeng@sunmi.com>
2019-08-27 19:35:52 +0800

    add string

--- app/src/main/java/com/sunmi/assistant/mine/message/MsgConstants.java
+++ app/src/main/java/com/sunmi/assistant/mine/message/MsgConstants.java
@@ -7,15 +7,9 @@ package com.sunmi.assistant.mine.message;
  */
 public class MsgConstants {
 
-    public static final String NOTIFY_MODEL_IPC = "notif-model-ipc";
     public static final String NOTIFY_DEVICE_IPC = "notif-model-device-ipc";
-    public static final String NOTIFY_IPC_SETTING = "notif-device-ipc-reminder-setting";
-    public static final String NOTIFY_MODEL_ESL = "notif-model-esl";
     public static final String NOTIFY_DEVICE_ESL = "notif-model-device-esl";
-    public static final String NOTIFY_ESL_SETTING = "notif-device-esl-reminder-setting";
-    public static final String NOTIFY_MODEL_SERVICE = "notif-model-service";
     public static final String NOTOFY_SERVICE_SETTING = "notif-system-service-reminder-setting";
-    public static final String NOTIFY_MODEL_TASK = "notif-model-task";
     public static final String NOTIFY_SYSTEM_TASK = "notif-model-system-task";
     public static final String NOTIFY_TASK_SETTING = "notif-system-task-reminder-setting";
     public static final String NOTIFY_PROMOTION_SETTING = "notif-system-promotion-reminder-setting";
