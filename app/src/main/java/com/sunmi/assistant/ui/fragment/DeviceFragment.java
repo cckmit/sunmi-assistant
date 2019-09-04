60f4ca74c67e7c79bab8c11fd0013c70901cdb1d
bruce yang <>
2019-09-04 10:52:21 +0800

    bug fix

--- app/src/main/java/com/sunmi/assistant/ui/fragment/DeviceFragment.java
+++ app/src/main/java/com/sunmi/assistant/ui/fragment/DeviceFragment.java
@@ -635,7 +635,7 @@ public class DeviceFragment extends BaseMvpFragment<DevicePresenter>
 
     @UiThread
     void showEmptyView() {
-        if (rlNoDevice == null) return;
+        if (rlNoDevice == null || btnAdd == null) return;
         if (deviceList.size() > 0) {
             rlNoDevice.setVisibility(View.GONE);
             btnAdd.setVisibility(View.VISIBLE);
