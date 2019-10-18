c93e40fe9ebc27adef5329af1fa48222962a6e9e
bruce yang <>
2019-10-18 17:35:56 +0800

    refactor color

--- app/src/main/java/com/sunmi/assistant/dashboard/card/CustomerNoFsCard.java
+++ app/src/main/java/com/sunmi/assistant/dashboard/card/CustomerNoFsCard.java
@@ -71,7 +71,7 @@ public class CustomerNoFsCard extends BaseRefreshCard<CustomerNoFsCard.Model, Ob
     public BaseViewHolder<Model> onCreateViewHolder(@NonNull View view, @NonNull ItemType<Model, BaseViewHolder<Model>> type) {
         BaseViewHolder<Model> holder = super.onCreateViewHolder(view, type);
         Context context = holder.getContext();
-        this.mColorGray = ContextCompat.getColor(context, R.color.color_F5F7FA);
+        this.mColorGray = ContextCompat.getColor(context, R.color.common_fill);
         this.mColorWhite = 0xFFFFFFFF;
         float radius = context.getResources().getDimension(R.dimen.dp_12);
         this.mContentBg = new GradientDrawable();
