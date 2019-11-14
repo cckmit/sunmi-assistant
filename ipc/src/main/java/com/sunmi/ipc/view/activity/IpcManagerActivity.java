b76df0210e1031f98f76bd9b40ff7af2b733a035
林远鹏 <linyuanpeng@sunmi.com>
2019-11-14 15:18:14 +0800

    fix bug

--- ipc/src/main/java/com/sunmi/ipc/view/activity/IpcManagerActivity.java
+++ ipc/src/main/java/com/sunmi/ipc/view/activity/IpcManagerActivity.java
@@ -1385,9 +1385,7 @@ public class IpcManagerActivity extends BaseMvpActivity<IpcManagerPresenter>
                 holder.setText(R.id.tv_summary, bean.getSummary());
                 holder.setText(R.id.btn_detail, bean.getRightText());
                 btnDetail.setEnabled(bean.isEnabled());
-                if (bean.getTagImageResId() != -1) {
-                    holder.setImageResource(R.id.iv_tag, bean.getTagImageResId());
-                }
+                holder.setImageResource(R.id.iv_tag, bean.getTagImageResId());
                 btnDetail.setOnClickListener(v -> {
                     if (bean.getLeftImageResId() == R.mipmap.ipc_cloud_storage) {
                         if (bean.getStatus() == CommonConstants.CLOUD_STORAGE_NOT_OPENED) {
