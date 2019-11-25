2a8fb21e67d459235747450127a574ff41800b08
yinhui <yinhui@sunmi.com>
2019-11-25 20:28:02 +0800

    [CloudVideo] Add calendar.

---
+++ ipc/src/main/java/com/sunmi/ipc/calendar/OnCalendarSelectListener.java
@@ -0,0 +1,11 @@
+package com.sunmi.ipc.calendar;
+
+import java.util.Calendar;
+
+/**
+ * @author yinhui
+ * @date 2019-11-25
+ */
+public interface OnCalendarSelectListener {
+    void onCalendarSelect(Calendar calendar);
+}
