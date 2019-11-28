5dcdd9cfb953ced61f461929c40a3b9e8946e057
yinhui <yinhui@sunmi.com>
2019-11-28 10:09:53 +0800

    [CloudVideo] Update time line fix length of line error.

--- ipc/src/main/java/com/sunmi/ipc/view/activity/CloudPlaybackActivity.java
+++ ipc/src/main/java/com/sunmi/ipc/view/activity/CloudPlaybackActivity.java
@@ -76,7 +76,7 @@ public class CloudPlaybackActivity extends BaseMvpActivity<CloudPlaybackPresente
         implements CloudPlaybackContract.View, IVideoPlayer.VideoPlayListener,
         ZFTimeLine.OnZFTimeLineListener, View.OnClickListener, VolumeHelper.VolumeChangeListener {
 
-    private final static long SECONDS_IN_ONE_DAY = 24 * 60 * 60;//3天秒数
+    private final static long SECONDS_IN_ONE_DAY = 24 * 60 * 60;
     private final static int tenMinutes = 10 * 60;//10分钟
 
     @ViewById(resName = "rl_screen")
@@ -724,7 +724,7 @@ public class CloudPlaybackActivity extends BaseMvpActivity<CloudPlaybackPresente
 
     @UiThread
     void refreshScaleTimePanel() {
-        timeLine.setBound(startTimeCurrentDate, endTimeCurrentDate);
+        timeLine.setBound(startTimeCurrentDate, startTimeCurrentDate + SECONDS_IN_ONE_DAY);
         timeLine.setVideoData(timeSlotsInDay);
         timeLine.refresh();
     }
