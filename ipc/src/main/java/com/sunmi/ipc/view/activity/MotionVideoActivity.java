f207d7588f4ce4c6ec10760e79398c62d1bd26b4
yinhui <yinhui@sunmi.com>
2019-12-10 11:27:22 +0800

    [MotionVideo] Clean Code

---
+++ ipc/src/main/java/com/sunmi/ipc/view/activity/MotionVideoActivity.java
@@ -0,0 +1,10 @@
+package com.sunmi.ipc.view.activity;
+
+import sunmi.common.base.BaseActivity;
+
+/**
+ * @author yinhui
+ * @date 2019-12-10
+ */
+public class MotionVideoActivity extends BaseActivity {
+}
