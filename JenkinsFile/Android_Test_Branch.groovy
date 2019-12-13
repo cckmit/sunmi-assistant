db93d7bd103ad1d250dff7d837679a7bf6dc1a4a
宁儒林 <ningrulin@sunmi.com>
2019-12-13 10:19:57 +0000

    更新 Android_Test_Branch.groovy

--- JenkinsFile/Android_Test_Branch.groovy
+++ JenkinsFile/Android_Test_Branch.groovy
@@ -25,8 +25,8 @@ pipeline{
                 mkdir -p build
                 fastlane testEnv
                 ''')
-              archiveArtifacts(artifacts: 'app/build/outputs/apk/**/app-myapp-universal-debug.apk', onlyIfSuccessful: true)
-              stash(includes: 'app/build/outputs/apk/**/app-myapp-universal-debug.apk', name: 'apk')
+              archiveArtifacts(artifacts: 'app/build/outputs/apk/**/app-myapp-universal-smtest.apk', onlyIfSuccessful: true)
+              stash(includes: 'app/build/outputs/apk/**/app-myapp-universal-smtest.apk', name: 'apk')
             }catch(e){
               def stageName = 'build'
               if(currentBuild.currentResult == "FAILURE"){
