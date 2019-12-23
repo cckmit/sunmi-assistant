716e48c55b64e6e743457e008638721f72801080
yangshijie <yangshijie@sunmi.com>
2019-12-23 18:10:38 +0800

    add protocol

---
+++ commonlib/src/main/java/sunmi/common/rpc/cloud/NetworkInterface.java
@@ -0,0 +1,33 @@
+package sunmi.common.rpc.cloud;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+import sunmi.common.rpc.retrofit.BaseRequest;
+import sunmi.common.rpc.retrofit.BaseResponse;
+
+/**
+ * @author yangShiJie
+ * @date 2019-12-23
+ */
+public interface NetworkInterface {
+    String URL = "/network/api/network/";
+
+    /**
+     * 获取 一段时间内网络实时事件(设备上下线，与saas连接网络延迟高,公网延迟高等
+     */
+    @POST(URL + "getEventList")
+    Call<BaseResponse<Object>> getEventList(@Body BaseRequest request);
+
+    /**
+     * 获取 最新的网路健康数据(公网延迟，saas延迟，干扰)
+     */
+    @POST(URL + "getHealthInfo")
+    Call<BaseResponse<Object>> getHealthInfo(@Body BaseRequest request);
+
+    /**
+     * app一段时间内的saas和公网延迟数据
+     */
+    @POST(URL + "getNetHealthList")
+    Call<BaseResponse<Object>> getNetHealthList(@Body BaseRequest request);
+}
