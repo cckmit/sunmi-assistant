fdf24abfcf3f39ba062454ad42c5d6c65586e855
linyuanpeng <linyuanpeng@sunmi.com>
2020-03-02 23:43:15 +0800

    数据总览页

--- app/src/main/java/com/sunmi/assistant/dashboard/card/total/TotalRealTimeOverviewCard.java
+++ app/src/main/java/com/sunmi/assistant/dashboard/card/total/TotalRealTimeOverviewCard.java
@@ -2,25 +2,37 @@ package com.sunmi.assistant.dashboard.card.total;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.sunmi.assistant.R;
 import com.sunmi.assistant.dashboard.card.BaseRefreshCard;
+import com.sunmi.assistant.dashboard.util.Utils;
 
 import retrofit2.Call;
 import sunmi.common.base.recycle.BaseViewHolder;
+import sunmi.common.model.CustomerCountResp;
+import sunmi.common.model.CustomerDataResp;
+import sunmi.common.model.SaleDataResp;
+import sunmi.common.rpc.cloud.SunmiStoreApi;
 import sunmi.common.rpc.retrofit.BaseResponse;
+import sunmi.common.rpc.retrofit.RetrofitCallback;
+import sunmi.common.utils.DateTimeUtils;
 
 public class TotalRealTimeOverviewCard extends BaseRefreshCard<TotalRealTimeOverviewCard.Model, Object> {
 
     private static TotalRealTimeOverviewCard sInstance;
+    private String startTime = DateTimeUtils.getYesterday("yyyy-MM-dd");
 
     private TotalRealTimeOverviewCard(Presenter presenter, int source) {
         super(presenter, source);
     }
 
-    public TotalRealTimeOverviewCard get(Presenter presenter, int source){
-        if (sInstance == null){
-            sInstance = new TotalRealTimeOverviewCard(presenter,source);
-        }else {
+    public TotalRealTimeOverviewCard get(Presenter presenter, int source) {
+        if (sInstance == null) {
+            sInstance = new TotalRealTimeOverviewCard(presenter, source);
+        } else {
             sInstance.reset(presenter, source);
         }
         return sInstance;
@@ -34,17 +46,88 @@ public class TotalRealTimeOverviewCard extends BaseRefreshCard<TotalRealTimeOver
 
     @Override
     public int getLayoutId(int type) {
-        return 0;
+        return R.layout.dashboard_item_total_realtime_overview;
     }
 
     @Override
     protected Call<BaseResponse<Object>> load(int companyId, int shopId, int period, CardCallback callback) {
+        //ToDo 通过source判断显示的数据
+        loadCustomerLatest(companyId, callback);
         return null;
     }
 
+    //今日总客流量
+    private void loadCustomerLatest(int companyId, CardCallback callback) {
+        SunmiStoreApi.getInstance().getTotalCustomerLatest(companyId, new RetrofitCallback<CustomerCountResp>() {
+            @Override
+            public void onSuccess(int code, String msg, CustomerCountResp data) {
+                Model model = getModel();
+                model.count = data.getLatestCount();
+                loadCustomerYesterday(companyId, callback);
+            }
+
+            @Override
+            public void onFail(int code, String msg, CustomerCountResp data) {
+                callback.onFail(code, msg, data);
+            }
+        });
+    }
+
+    private void loadCustomerYesterday(int companyId, CardCallback callback) {
+        SunmiStoreApi.getInstance().getTotalCustomerData(companyId, startTime, 1, new RetrofitCallback<CustomerDataResp>() {
+            @Override
+            public void onSuccess(int code, String msg, CustomerDataResp data) {
+                Model model = getModel();
+                model.earlCount = data.getPassengerCount();
+                loadSaleData(companyId, callback);
+            }
+
+            @Override
+            public void onFail(int code, String msg, CustomerDataResp data) {
+                callback.onFail(code, msg, data);
+            }
+        });
+    }
+
+    //今日销售额和总交易数
+    private void loadSaleData(int companyId, CardCallback callback) {
+        SunmiStoreApi.getInstance().getTotalSaleData(companyId, new RetrofitCallback<SaleDataResp>() {
+            @Override
+            public void onSuccess(int code, String msg, SaleDataResp data) {
+                Model model = getModel();
+                model.amount = data.getLatestOrderAmount();
+                model.volume = data.getLatestOrderCount();
+                loadSaleDataYesterday(companyId, callback);
+            }
+
+            @Override
+            public void onFail(int code, String msg, SaleDataResp data) {
+                callback.onFail(code, msg, data);
+            }
+        });
+    }
+
+    private void loadSaleDataYesterday(int companyId, CardCallback callback) {
+        SunmiStoreApi.getInstance().getTotalHistorySaleData(companyId, startTime, 1, new RetrofitCallback<SaleDataResp>() {
+            @Override
+            public void onSuccess(int code, String msg, SaleDataResp data) {
+                Model model = getModel();
+                model.earlAmount = data.getOrderAmount();
+                model.earlCount = data.getOrderCount();
+                callback.onSuccess();
+            }
+
+            @Override
+            public void onFail(int code, String msg, SaleDataResp data) {
+                callback.onFail(code, msg, data);
+            }
+        });
+    }
+
+
     @Override
     protected Model createModel() {
-        return null;
+        return new Model();
     }
 
     @Override
@@ -54,10 +137,80 @@ public class TotalRealTimeOverviewCard extends BaseRefreshCard<TotalRealTimeOver
 
     @Override
     protected void setupView(@NonNull BaseViewHolder<Model> holder, Model model, int position) {
+        Context context = holder.getContext();
+        TextView value = holder.getView(R.id.tv_dashboard_value);
+        TextView subValue = holder.getView(R.id.tv_dashboard_subdata);
+        TextView amount = holder.getView(R.id.tv_sales_amount);
+        TextView amountSubData = holder.getView(R.id.tv_sales_amount_subdata);
+        TextView volume = holder.getView(R.id.tv_sales_volume);
+        TextView volumeSubData = holder.getView(R.id.tv_sales_volume_subdata);
+        value.setText(model.getCount(context));
+        subValue.setText(model.getEarlCount(context));
+        amount.setText(model.getAmount(context));
+        amountSubData.setText(model.getEarlAmount(context));
+        volume.setText(model.getVolume(context));
+        volumeSubData.setText(model.getEarlVolume(context));
+    }
 
+    @Override
+    protected void showLoading(@NonNull BaseViewHolder<Model> holder, Model model, int position) {
+        View main = holder.getView(R.id.layout_dashboard_main);
+        View amount = holder.getView(R.id.layout_sales_amount);
+        View volume = holder.getView(R.id.layout_sales_volume);
+        ImageView loading = holder.getView(R.id.iv_dashboard_loading);
+        loading.setImageResource(R.mipmap.dashboard_skeleton_multi);
+        main.setVisibility(View.INVISIBLE);
+        amount.setVisibility(View.INVISIBLE);
+        volume.setVisibility(View.INVISIBLE);
+        loading.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    protected void showError(@NonNull BaseViewHolder<Model> holder, Model model, int position) {
+        TextView value = holder.getView(R.id.tv_dashboard_value);
+        TextView subValue = holder.getView(R.id.tv_dashboard_subdata);
+        TextView amount = holder.getView(R.id.tv_sales_amount);
+        TextView amountSubData = holder.getView(R.id.tv_sales_amount_subdata);
+        TextView volume = holder.getView(R.id.tv_sales_volume);
+        TextView volumeSubData = holder.getView(R.id.tv_sales_volume_subdata);
+        value.setText(Utils.DATA_NONE);
+        subValue.setText(Utils.DATA_NONE);
+        amount.setText(Utils.DATA_NONE);
+        amountSubData.setText(Utils.DATA_NONE);
+        volume.setText(Utils.DATA_NONE);
+        volumeSubData.setText(Utils.DATA_NONE);
     }
 
     public static class Model extends BaseRefreshCard.BaseModel {
+        int count;
+        int earlCount;
+        double amount;
+        double earlAmount;
+        int volume;
+        int earlVolume;
 
+        private CharSequence getCount(Context context) {
+            return Utils.formatNumber(context, count, false, true);
+        }
+
+        private CharSequence getEarlCount(Context context) {
+            return Utils.formatNumber(context, earlCount, false, false);
+        }
+
+        private CharSequence getAmount(Context context) {
+            return Utils.formatNumber(context, amount, true, true);
+        }
+
+        private CharSequence getEarlAmount(Context context) {
+            return Utils.formatNumber(context, earlAmount, true, false);
+        }
+
+        private CharSequence getVolume(Context context) {
+            return Utils.formatNumber(context, volume, false, true);
+        }
+
+        private CharSequence getEarlVolume(Context context) {
+            return Utils.formatNumber(context, earlVolume, false, false);
+        }
     }
 }
