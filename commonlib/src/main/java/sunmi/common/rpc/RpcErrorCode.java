4d1f9d8d483c6c16f4600dd5b242ee6a6435daa3
bruce yang <>
2020-03-13 18:35:26 +0800

    add error code

--- commonlib/src/main/java/sunmi/common/rpc/RpcErrorCode.java
+++ commonlib/src/main/java/sunmi/common/rpc/RpcErrorCode.java
@@ -8,6 +8,8 @@ public class RpcErrorCode {
     public static final int RPC_COMMON_ERROR = 990; //访问失败或无网络
     public static final int RPC_ERR_TIMEOUT = 991; //自定义的请求超时
 
+    public static final int OPCODE_RESPONSE_NULL = 980; //opcode请求返回为空
+
     //云端http请求 网关错误
     public static int HTTP_RESP_UNKNOWN_REQUEST = 400;//服务器不理解请求的语法
     public static int HTTP_RESP_TOKEN_ERR = 401;//缺少或者非法token
