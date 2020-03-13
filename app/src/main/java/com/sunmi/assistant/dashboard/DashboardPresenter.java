62d5cd6ca3f4cd50b631a231006c994caa6ce424
yinhui <yinhui@sunmi.com>
2020-03-13 18:28:03 +0800

    APP-4527 【Android】在子门店的数据页的添加AI摄像机的入口绑定绑定完摄像机之后，返回数据页，发现该入口依然存在

--- app/src/main/java/com/sunmi/assistant/dashboard/DashboardPresenter.java
+++ app/src/main/java/com/sunmi/assistant/dashboard/DashboardPresenter.java
@@ -67,7 +67,7 @@ class DashboardPresenter extends BasePresenter<DashboardContract.View>
     @Override
     public DashboardCondition onChildCreate(PageContract.PagePresenter presenter) {
         mPages.put(presenter.getType(), presenter);
-        return mCondition;
+        return mCondition == null ? null : mCondition.copy();
     }
 
     @Override
@@ -164,7 +164,7 @@ class DashboardPresenter extends BasePresenter<DashboardContract.View>
                 for (int i = 0, size = mPages.size(); i < size; i++) {
                     PageContract.PagePresenter page = mPages.valueAt(i);
                     LogCat.i(Utils.TAG, "Page:" + page.getType());
-                    page.setCondition(mCondition);
+                    page.setCondition(mCondition.copy());
                     if (!onlyCurrentPage || page.getType() == mPageType) {
                         page.refresh(showLoading);
                     }
